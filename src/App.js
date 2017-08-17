7e057f62efa1f159814222b964d20bb3a5f2ef7a
ginpei <ginpei@gmail.com>
2017-08-16 23:08:04 -0700

    no need to use template

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import secret from '../secret.json';
 
 if (firebase.apps.length < 1) {
 	firebase.initializeApp({
-		apiKey: `${secret.API_KEY}`,
+		apiKey: secret.API_KEY,
 		authDomain: `${secret.PROJECT_ID}.firebaseapp.com`,
 		databaseURL: `https://${secret.PROJECT_ID}.firebaseio.com`,
 		storageBucket: `${secret.PROJECT_ID}.appspot.com`,
