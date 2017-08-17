55ee19bc888fbdb8bb2a1a923f966a83c45179fb
ginpei <ginpei@gmail.com>
2017-08-16 20:16:16 -0700

    UIs

--- index.android.js
+++ index.android.js
@@ -6,48 +6,120 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	Button,
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 
+function StatusText({ label, value }) {
+	const styles = StyleSheet.create({
+		row: {
+			display: 'flex',
+			flexDirection: 'row',
+		},
+		label: {
+			flex: 1,
+			padding: 8,
+			textAlign: 'right',
+		},
+		value: {
+			flex: 1,
+			padding: 8,
+		},
+	});
+
+	let valueText;
+	if (typeof value === 'boolean') {
+		valueText = value ? 'Yes' : 'No';
+	}
+	else {
+		valueText = String(value);
+	}
+
+	return (
+		<View style={styles.row}>
+			<Text style={styles.label}>{label}</Text>
+			<Text style={styles.value}>{valueText}</Text>
+		</View>
+	);
+}
+
 export default class ReactNativeFirebaseAuth extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			signedIn: false,
+		};
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.statusSection}>
+					<StatusText
+						label="Signed in?"
+						value={this.state.signedIn}
+						/>
+					<StatusText
+						label="Anonymous?"
+						value={false}
+						/>
+					<StatusText
+						label="Email"
+						value={null}
+						/>
+				</View>
+				<View style={styles.buttonLine}>
+					<Button
+						title="Sign in (anonymous)"
+						disabled={this.state.signedIn}
+						onPress={_ => console.debug('Sgin in (anonymous)')}
+						/>
+				</View>
+				<View style={styles.buttonLine}>
+					<Button
+						title="Sign in (email)"
+						disabled={this.state.signedIn}
+						onPress={_ => console.debug('Sgin in (email)')}
+						/>
+				</View>
+				<View style={styles.buttonLine}>
+					<Button
+						title="Sign out"
+						disabled={!this.state.signedIn}
+						onPress={_ => console.debug('Sgin out')}
+						/>
+				</View>
+				<View style={styles.buttonLine}>
+					<Button
+						title="Save signing in status"
+						onPress={_ => console.debug('save')}
+						/>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		alignItems: 'center',
+		backgroundColor: '#fff',
+		flex: 1,
+		justifyContent: 'center',
+	},
+	statusSection: {
+		marginBottom: 32,
+		width: '100%',
+	},
+	buttonLine: {
+		marginBottom: 16,
+		minWidth: '50%',
+		paddingLeft: 16,
+		paddingRight: 16,
+	},
 });
 
 AppRegistry.registerComponent('ReactNativeFirebaseAuth', () => ReactNativeFirebaseAuth);
